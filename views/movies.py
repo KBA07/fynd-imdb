a19af594f9b7315ece7701a71b64894809b1fbdf
KBA07 <kashif.bilal.ali07@gmail.com>
2021-01-16 22:29:56 +0530

    Adding a validator class and handling 500 for the get request

--- views/movies.py
+++ views/movies.py
@@ -7,26 +7,14 @@ from flask import Blueprint, request
 
 from data_api.movies_dao import parse_json, movie_id_exists, \
     movie_exists, add_movie, delete_movie_from_db, edit_movie, get_movie, get_genre_list
+
+from helpers.auth import basic_auth
 from helpers.db import terminating_sn
 from helpers.logger import LOG
-from helpers.auth import basic_auth
 from helpers.response_maker import ResponseMaker
+from helpers.validator import Validator, InputOutOfBounds
 
 blueprint = Blueprint('movies', __name__)
-
-MAX_LIMIT = 100
-DEFAULT_LIMIT = 20
-DEFAULT_OFFSET = 0
-
-
-def validate_get_limit_offset(limit, offset):
-    if limit and limit > MAX_LIMIT or limit < 0:
-        limit = MAX_LIMIT
-
-    if not offset or offset < 0:
-        offset = 0
-
-    return limit, offset
 
 
 @blueprint.route('/')
@@ -68,23 +56,30 @@ def get_movies():
     name = request.args.get('name')
     director = request.args.get('director')
     genre = request.args.get('genre')
-    limit = int(request.args.get('limit', DEFAULT_LIMIT))
-    offset = int(request.args.get('offset', DEFAULT_OFFSET))
+    limit = int(request.args.get('limit', Validator.DEFAULT_LIMIT))
+    offset = int(request.args.get('offset', Validator.DEFAULT_OFFSET))
 
-    limit, offset = validate_get_limit_offset(limit, offset)
+    limit, offset = Validator.get_limit_offset(limit, offset)
 
-    with terminating_sn() as session:
-        total, resp = get_movie(session, name, director, genre, limit, offset)
+    try:
+        with terminating_sn() as session:
+            total, resp = get_movie(session, name, director, genre, limit, offset)
 
-        movie_list = []
-        for movie in resp:
-            movie_id, popularity, director, genre_blob, imdb_score, name = movie
-            movie_list.append({'id': movie_id, '99popularity': popularity,
-                               'director': director, 'genre': get_genre_list(genre_blob),
-                               'imdb_score': imdb_score, 'name': name})
+            movie_list = []
+            for movie in resp:
+                movie_id, popularity, director, genre_blob, imdb_score, name = movie
+                movie_list.append({'id': movie_id, '99popularity': popularity,
+                                   'director': director, 'genre': get_genre_list(genre_blob),
+                                   'imdb_score': imdb_score, 'name': name})
 
-        resp = {'total': total, 'data': movie_list}
-        return ResponseMaker(ResponseMaker.RESPONSE_200).return_response(resp)
+            resp = {'total': total, 'data': movie_list}
+            return ResponseMaker(ResponseMaker.RESPONSE_200).return_response(resp)
+    except Exception:
+        session.rollback()
+        LOG.exception("Exception occurred while writting movie {} to db".format(name))
+        return ResponseMaker(ResponseMaker.RESPONSE_500).return_response(
+            ResponseMaker.RESPONSE_500_MESSAGE)
+
 
 
 @blueprint.route('/v1/movies', methods=['POST'])
@@ -117,13 +112,14 @@ def add_movies():
     if data:
         popularity, director, genre_list, imdb_score, name = parse_json(data)
 
-    # Add a validation for popularity and imdb_score
-
-    if not all([popularity, director, genre_list, imdb_score, name]):
-        return ResponseMaker(ResponseMaker.RESPONSE_400, ResponseMaker.RESPONSE_400_MESSAGE,
-                             ResponseMaker.RESPONSE_400_ERROR_MISSING_FIELDS).return_response()
-
     try:
+        # Add a validation for popularity and imdb_score
+        Validator.validate_param(popularity, imdb_score)
+
+        if not all([popularity, director, genre_list, imdb_score, name]):
+            return ResponseMaker(ResponseMaker.RESPONSE_400, ResponseMaker.RESPONSE_400_MESSAGE,
+                                 ResponseMaker.RESPONSE_400_ERROR_MISSING_FIELDS).return_response()
+
         with terminating_sn() as session:
             if movie_exists(session, name):
                 return ResponseMaker(ResponseMaker.RESPONSE_400,
@@ -134,6 +130,10 @@ def add_movies():
             add_movie(session, popularity, director, genre_list, imdb_score, name)
             return ResponseMaker(ResponseMaker.RESPONSE_200).return_response(
                 ResponseMaker.RESPONSE_200_MESSAGE)
+
+    except InputOutOfBounds:
+        return ResponseMaker(ResponseMaker.RESPONSE_400, ResponseMaker.RESPONSE_400_MESSAGE,
+                             ResponseMaker.RESPONSE_400_ERROR_OUT_OF_BOUNDS).return_response()
     except Exception:
         session.rollback()
         LOG.exception("Exception occurred while writting movie {} to db".format(name))
@@ -176,13 +176,14 @@ def edit_movies():
     if data:
         popularity, director, genre_list, imdb_score, name = parse_json(data)
 
-    # Add a validation for popularity and imdb_score
-
-    if not movie_id or not any([popularity, director, genre_list, imdb_score, name]):
-        return ResponseMaker(ResponseMaker.RESPONSE_400, ResponseMaker.RESPONSE_400_MESSAGE,
-                             ResponseMaker.RESPONSE_400_ERROR_MISSING_FIELDS).return_response()
-
     try:
+        # Add a validation for popularity and imdb_score
+        Validator.validate_param(popularity, imdb_score)
+
+        if not movie_id or not any([popularity, director, genre_list, imdb_score, name]):
+            return ResponseMaker(ResponseMaker.RESPONSE_400, ResponseMaker.RESPONSE_400_MESSAGE,
+                                 ResponseMaker.RESPONSE_400_ERROR_MISSING_FIELDS).return_response()
+
         with terminating_sn() as session:
             if not movie_id_exists(session, movie_id):
                 return ResponseMaker(ResponseMaker.RESPONSE_400,
@@ -193,6 +194,10 @@ def edit_movies():
             edit_movie(session, movie_id, popularity, director, genre_list, imdb_score, name)
             return ResponseMaker(ResponseMaker.RESPONSE_200).return_response(
                 ResponseMaker.RESPONSE_200_MESSAGE)
+
+    except InputOutOfBounds:
+        return ResponseMaker(ResponseMaker.RESPONSE_400, ResponseMaker.RESPONSE_400_MESSAGE,
+                             ResponseMaker.RESPONSE_400_ERROR_OUT_OF_BOUNDS).return_response()
     except Exception:
         session.rollback()
         LOG.exception("Exception occurred while editing a movie if {} info".format(movie_id))
