b859eadb1fac9a0654e07e6af3f649a226819b82
KBA07 <kashif.bilal.ali07@gmail.com>
2023-05-30 12:45:26 +0530

    Commit for deployment on AWS

--- webapp.py
+++ webapp.py
@@ -1,22 +1,31 @@
 from flask import Flask
 
-# from helpers.db import load_db
-# from helpers.parser import Parser
+from helpers.db import load_db
+from helpers.parser import Parser
+
 from views.movies import blueprint
 
 
-def create_app():
-    # Create DB
-    # load_db()
+def register_flask_blueprint(app=None, init_db=False):
 
-    # parser = Parser('imdb.json')
-    # parser.populate()
+    if init_db:
+        # Create DB and initialize
+        load_db()
 
-    # Instantiate flask app
-    app = Flask(__name__)
+        parser = Parser('imdb.json')
+        parser.populate()
+    
+    if not app:
+        # Instantiate flask app
+        app = Flask(__name__)
 
     # Register Blueprint
-    app.register_blueprint(blueprint)
+    app.register_blueprint(blueprint, url_prefix='/portfolios/fynd-imdb')
 
     # Return app
     return app
+
+
+if __name__ == "__main__":
+    app = register_flask_blueprint(None, True)
+    app.run(debug=True)
